031701f25ac61445c774737e7bcbdeb69e4ebdde
Timothée Perdriau <perdriau@gmail.com>
2020-05-10 17:07:06 +0200

    Solving constructor issue

--- rest-user/src/main/java/org/timeflies/users/UsersResource.java
+++ rest-user/src/main/java/org/timeflies/users/UsersResource.java
@@ -25,16 +25,13 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("/api/users")
 @Produces(APPLICATION_JSON)
 public class UsersResource {
+
     private static final Logger log = Logger.getLogger(UsersResource.class);
 
-    private final UsersService service;
     @ConfigProperty(name = "users.default.sort", defaultValue = "lastName,firstName,userName")
     public String sort;
-
     @Inject
-    public UsersResource(UsersService service) {
-        this.service = service;
-    }
+    UsersService service;
 
     @Operation(summary = "Returns a random user")
     @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Users.class, required = true)))
