af5aa9ee0555f95f33dd2f2ff7ee905d54291879
Timothée Perdriau <perdriau@gmail.com>
2020-05-11 11:56:55 +0200

    first tests

--- rest-project/src/test/java/org/timeflies/projects/ProjectResourceTest.java
+++ rest-project/src/test/java/org/timeflies/projects/ProjectResourceTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -17,10 +20,52 @@ public class ProjectResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-                .when().get("/api/projects")
+                .when().get("/api/projects/hello")
                 .then()
                 .statusCode(200)
-                .body(is("hello"));
+                .body(is("hello " + ProjectResource.class.getSimpleName()));
+    }
+
+    @Test
+    void shouldPingOpenAPI() {
+        given()
+                .header(ACCEPT, APPLICATION_JSON)
+                .when().get("/openapi")
+                .then()
+                .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void shouldPingSwaggerUI() {
+        given()
+                .when().get("/swagger-ui")
+                .then()
+                .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void shouldPingLiveness() {
+        given()
+                .when().get("/health/live")
+                .then()
+                .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void shouldPingReadiness() {
+        given()
+                .when().get("/health/ready")
+                .then()
+                .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void shouldPingMetrics() {
+        given()
+                .header(ACCEPT, APPLICATION_JSON)
+                .when().get("/metrics/application")
+                .then()
+                .statusCode(OK.getStatusCode());
     }
 
 }
